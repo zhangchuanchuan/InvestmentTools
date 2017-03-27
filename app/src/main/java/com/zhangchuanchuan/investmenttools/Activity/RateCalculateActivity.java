12acc8f5f957d7b1e66de14d27f09588324d64b2
zhangchuanchuan <543749092@qq.com>
2017-03-27 22:49:57 +0800

    添加输入输出元素

--- app/src/main/java/com/zhangchuanchuan/investmenttools/Activity/RateCalculateActivity.java
+++ app/src/main/java/com/zhangchuanchuan/investmenttools/Activity/RateCalculateActivity.java
@@ -3,6 +3,8 @@ package com.zhangchuanchuan.investmenttools.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.zhangchuanchuan.investmenttools.R;
+
 /**
  * Created by zhangchuanchuan on 17-3-26.
  * 利率计算的Activity
@@ -12,6 +14,7 @@ public class RateCalculateActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_calculatie);
 
     }
 }
