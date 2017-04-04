84c2721c6409b257fed536eb4cd52a23c173e6a7
zhangchuanchuan <543749092@qq.com>
2017-04-04 19:27:21 +0800

    计算

--- app/src/main/java/com/zhangchuanchuan/investmenttools/activity/RateCalculateActivity.java
+++ app/src/main/java/com/zhangchuanchuan/investmenttools/activity/RateCalculateActivity.java
@@ -2,9 +2,16 @@ package com.zhangchuanchuan.investmenttools.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.zhangchuanchuan.investmenttools.R;
 import com.zhangchuanchuan.investmenttools.utils.RateCalculateUtils;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by zhangchuanchuan on 17-3-26.
@@ -12,11 +19,57 @@ import com.zhangchuanchuan.investmenttools.utils.RateCalculateUtils;
  */
 
 public class RateCalculateActivity extends BaseActivity {
+
+    //input
+    private Spinner frequency;
+    private EditText money;
+    private EditText rate;
+    private EditText time;
+
+    //calculate
+    private Button calculate;
+
+    //output
+    private TextView investmentMoney;
+    private TextView totalMoney;
+    private TextView totalRate;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_calculatie);
-        RateCalculateUtils.getAllRate(0.05, 20);
+        frequency = (Spinner) findViewById(R.id.frequency);
+        money = (EditText) findViewById(R.id.money);
+        rate = (EditText) findViewById(R.id.rate_input);
+        time = (EditText) findViewById(R.id.time_number);
+
+        calculate = (Button) findViewById(R.id.calculate);
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculate();
+            }
+        });
+
+        investmentMoney = (TextView) findViewById(R.id.investment_money);
+        totalMoney = (TextView) findViewById(R.id.total_money);
+        totalRate = (TextView) findViewById(R.id.total_rate);
+
+    }
+
+    private void calculate() {
+        try {
+            double yearRate = Double.valueOf(rate.getText().toString());
+            int times = Integer.valueOf(time.getText().toString());
+            double allRate = RateCalculateUtils.getAllRate(yearRate, times);
+
+            int investMoney = (Integer.valueOf(money.getText().toString())) * times;
+            investmentMoney.setText(investMoney);
+            totalRate.setText(allRate * 100 + "");
+            totalMoney.setText(investMoney * allRate + "");
+        } catch (Exception e) {
+            investmentMoney.setText("输入有误");
+        }
     }
 
 
